df40b6fd9dc9f00203ffd20d27e6333360e22672
aroo22gitHub <thakshilarathnayake19@gmail.com>
2026-01-14 12:54:21 +0530

    T14: Implement Insertion Sort algorithm #17

---
+++ SortingAlgorithmEvaluator/src/main/java/com/sortingapp/sortingalgorithmevaluator/algorithms/InsertionSort.java
@@ -0,0 +1,25 @@
+package com.sortingapp.sortingalgorithmevaluator.algorithms;
+
+
+public class InsertionSort implements SortingAlgorithm {
+
+    @Override
+    public void sort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Insertion Sort";
+    }
+}
