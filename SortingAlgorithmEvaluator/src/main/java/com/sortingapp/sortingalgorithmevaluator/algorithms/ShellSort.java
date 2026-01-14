f634d5551c3ed18048e2268b48472a664fb6c07f
aroo22gitHub <thakshilarathnayake19@gmail.com>
2026-01-14 12:35:24 +0530

    T13: Implement Shell Sort algorithm #16

---
+++ SortingAlgorithmEvaluator/src/main/java/com/sortingapp/sortingalgorithmevaluator/algorithms/ShellSort.java
@@ -0,0 +1,25 @@
+package com.sortingapp.sortingalgorithmevaluator.algorithms;
+
+public class ShellSort implements SortingAlgorithm {
+
+    @Override
+    public void sort(int[] array) {
+        int n = array.length;
+
+        for (int gap = n/2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Shell Sort";
+    }
+}
